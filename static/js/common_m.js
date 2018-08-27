d5175a18d099fa0ec47e78bdaf7b79034744fe80
강혜준 <vxggangxv@naver.com>
2018-08-27 18:17:58 +0900

    완료

--- static/js/common_m.js
+++ static/js/common_m.js
@@ -20,13 +20,26 @@ function headFunc () {
 function scrollFunc () {
 	var btnTop	= $(".btn-top"),
 //		top		= $(".main .slide").height();
-		top		= 320;
+		top		= 320,
+		hd = $(".header"),
+		hdHt = $(".header").height();
 	$(window).scroll(function() {
 		var scr = $(window).scrollTop();
+		if ( scr > hdHt ) {
+			hd.addClass("active");
+		} else {
+			hd.removeClass("active");
+		}
 		if ( scr > top ) {
 			btnTop.fadeIn();
 		} else {
 			btnTop.fadeOut();
 		}
+	});
+	btnTop.on("click", function() {
+		$("html, body").stop().animate({
+			scrollTop: 0
+		}, 500);
+		return false;
 	});
 }
